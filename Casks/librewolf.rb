1a9859a712321c357d948acf464ae86a093a03e3
tttt00000 <tibor0chab@gmail.com>
2026-06-03 17:55:01 +0000

    Add Cask for LibreWolf browser

---
+++ Casks/librewolf.rb
@@ -0,0 +1,43 @@
+cask "librewolf" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "151.0.3,1"
+  sha256 arm:   "b0bc6c7877ac47b0ae1a9ede8dca01cab5945e87323b20c679a5c3f9f37b9a13"
+
+  url "https://codeberg.org/api/packages/librewolf/generic/librewolf/#{version.tr(",", "-")}/librewolf-#{version.tr(",", "-")}-macos-#{arch}-package.dmg",
+      verified: "codeberg.org/api/packages/librewolf/generic/librewolf/"
+  name "LibreWolf"
+  desc "Web browser"
+  homepage "https://librewolf.net/"
+
+
+  depends_on :macos
+
+  app "LibreWolf.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/librewolf.wrapper.sh"
+  binary shimscript, target: "librewolf"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/LibreWolf.app/Contents/MacOS/librewolf' "$@"
+    EOS
+  end
+
+postflight do
+  system_command "/usr/bin/xattr",
+                 args: ["-d", "com.apple.quarantine", "#{appdir}/LibreWolf.app"],
+                 sudo: false
+end
+
+
+  zap trash: [
+    "~/.librewolf",
+    "~/Library/Application Support/LibreWolf",
+    "~/Library/Caches/LibreWolf Community",
+    "~/Library/Caches/LibreWolf",
+    "~/Library/Preferences/io.gitlab.librewolf-community.librewolf.plist",
+    "~/Library/Saved Application State/io.gitlab.librewolf-community.librewolf.savedState",
+  ]
+end
