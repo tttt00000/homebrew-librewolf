ad35974ff6b2e92fc98246643d169eb0124332f5
tttt00000 <tibor0chab@gmail.com>
2026-06-10 08:57:23 +0000

    Fix workflow and cask to handle both ARM64 and Intel sha256

--- Casks/librewolf.rb
+++ Casks/librewolf.rb
@@ -2,14 +2,14 @@ cask "librewolf" do
   arch arm: "arm64", intel: "x86_64"
 
   version "151.0.4,1"
-  sha256 arm:   "b0bc6c7877ac47b0ae1a9ede8dca01cab5945e87323b20c679a5c3f9f37b9a13"
+  sha256 arm:   "b0bc6c7877ac47b0ae1a9ede8dca01cab5945e87323b20c679a5c3f9f37b9a13",
+         intel: "6518fb4c4437c0cd629284005ac89c4fdf653e7fd8d1bc6146a0d644b2ec1592"
 
   url "https://codeberg.org/api/packages/librewolf/generic/librewolf/#{version.tr(",", "-")}/librewolf-#{version.tr(",", "-")}-macos-#{arch}-package.dmg",
       verified: "codeberg.org/api/packages/librewolf/generic/librewolf/"
   name "LibreWolf"
   desc "Web browser"
   homepage "https://librewolf.net/"
-
 
   depends_on :macos
 
